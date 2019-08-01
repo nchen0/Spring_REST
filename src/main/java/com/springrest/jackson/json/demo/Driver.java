cde85ac38f413206aea4dc9bd7326de71178268a
nchen0 <nicky.chen0@gmail.com>
2019-08-01 19:31:15 -0400

    jackson

--- src/main/java/com/springrest/jackson/json/demo/Driver.java
+++ src/main/java/com/springrest/jackson/json/demo/Driver.java
@@ -2,6 +2,8 @@ package com.springrest.jackson.json.demo;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.File;
+
 public class Driver {
     public static void main(String[] args) {
         try {
@@ -10,6 +12,11 @@ public class Driver {
 
             // read JSON file & convert to Java POJO
 
+            Student theStudent = mapper.readValue(new File("data/sample-full.json"), Student.class);
+
+            System.out.println("First name = " + theStudent.getFirstName());
+            Address tempAddress = theStudent.getAddress();
+            System.out.println("Street = " + tempAddress.getStreet());
 
             // print first name and last name
         } catch(Exception exc) {
